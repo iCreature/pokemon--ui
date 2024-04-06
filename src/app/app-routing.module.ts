ce31d90f4e54b570c8958f07d6357a1c39d4b926
iCreature <mxolisi1025@gmail.com>
2024-04-06 17:35:55 +0200

    project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokeListComponent } from './poke-list/poke-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : 'pokemoné', component : PokeListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
