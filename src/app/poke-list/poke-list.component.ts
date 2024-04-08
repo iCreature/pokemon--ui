ff7b7bde9796f17b2248424e6d5ff7ff2dac9672
iCreature <mxolisi1025@gmail.com>
2024-04-09 00:34:02 +0200

    details

--- src/app/poke-list/poke-list.component.ts
+++ src/app/poke-list/poke-list.component.ts
@@ -1,5 +1,6 @@
-import { Component , OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PokeapiService } from '../pokeapi.service';
+
 @Component({
   selector: 'app-poke-list',
   templateUrl: './poke-list.component.html',
@@ -9,20 +10,24 @@ export class PokeListComponent implements OnInit {
 
   constructor(private service: PokeapiService) {}
 
-  PokemonList!: any[];
+  PokemonList: any=[];
+  detailId: any; // variable to hold the detail id
 
   ngOnInit(): void {
-      this.getList();
+    this.getList();
   }
 
   getList() {
-    this.service.getPokemoneList().subscribe(data=>{
-      this.PokemonList=data;
+    this.service.getPokemoneList().subscribe(data => {
+      this.PokemonList = data;
     });
+  }
+
+  showDetails(id: any) {
+    this.detailId = id; // set the detail id when the button is clicked
   }
 
   getImageUrl(id: any) {
     return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
   }
-
 }
--- src/app/poke-list/pokemone-show/pokemone-show.component.ts
+++ src/app/poke-list/pokemone-show/pokemone-show.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component , OnInit, Input} from '@angular/core';
+import { PokeapiService } from 'src/app/pokeapi.service';
+
 
 @Component({
   selector: 'app-pokemone-show',
   templateUrl: './pokemone-show.component.html',
   styleUrls: ['./pokemone-show.component.scss']
 })
-export class PokemoneShowComponent {
+export class PokemoneShowComponent implements OnInit{
+
+  @Input() detailId:any;
+  DetailList:any=[];
+  
+  constructor(private service: PokeapiService) {}
+
+  ngOnInit(): void {
+      this.getDetails();
+  }
+
+  getDetails() {
+    this.service.getDetailed(this.detailId).subscribe(data => {
+      this.DetailList = data;
+    });
+  }
+
 
 }
